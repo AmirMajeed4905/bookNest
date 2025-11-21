67f354ecddff17751029aa0a4563ba50423048c9
Amir Majeed <amirmajeed4905@gmail.com>
2025-11-21 22:29:20 +0500

    product section...

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Hero from "../components/Hero";
 import CosmnaSection from "../components/CosmnaSection";
 import Footer from "../components/Footer";
+import ProductList from "../components/ProductList";
 
 const Product = () => {
   return (
     <div>
       <Hero />
       <CosmnaSection />
+      <ProductList />
       <Footer />
     </div>
   );
